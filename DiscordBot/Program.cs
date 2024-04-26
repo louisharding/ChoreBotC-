61338e0eddbe8027e68ee32d38353b6cb42011fb
Loui <lgharding0@gmail.com>
2024-04-26 12:29:42 +0100

    Add project files.

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+private DiscordSocketClient _client;
+    public static async Task Main(string[] args)
+    {
+        await new Program().RunBotAsync();
+    }
+}
