d007692206fa51d5d707eb03f33669fd7b48f412
Loui <lgharding0@gmail.com>
2024-05-02 12:46:51 +0100

    commit

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -2,13 +2,29 @@
 using Discord;
 using Discord.WebSocket;
 using System;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
+using static Program;
 
 class Program
 {
-private DiscordSocketClient _client;
-    public static async Task Main(string[] args)
+    static async Task Main(string[] args)
     {
-        await new Program().RunBotAsync();
+        Console.WriteLine("waiting for an asynchronous request to return...");
+        await completeAcynchronousTask();
+        Console.WriteLine("completed ");
+    }
+
+    static void myMethod()
+    {
+        Console.WriteLine("myMethod");
+    }
+
+
+    static async Task completeAcynchronousTask()
+    {
+        await Task.Delay(2000);
+        Console.WriteLine("Returning");
     }
 }
+
