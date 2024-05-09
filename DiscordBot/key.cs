e8e6080e1ea5fe1128547a9319d2daad2e784a67
Louis <lgharding0@gmail.com>
2024-05-09 14:33:36 +0100

    added gitignore and added discord server key to local class in seperate namespace file

---
+++ DiscordBot/key.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    internal class key
+    {
+       internal String serverKey = new string("MTE4MzA5MTE4NDUyOTMyMjAyNA.Gz5dhw.WcL_uR_8Br1vsIKgHjOPtFyPnpKFL_V-SXoxFk");
+    }
+}
