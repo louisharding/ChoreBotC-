32e981f9b81d38311919239302e18eb9c34bc376
Louis <lgharding0@gmail.com>
2024-05-22 17:06:51 +0100

    less now

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -7,49 +7,19 @@ using DiscordBot;
 
 class Program
 {
-    private DiscordSocketClient _client;
+    private static DiscordSocketClient _client;
+
+
     static async Task Main(string[] args)
     {
-        await new Program().RunBotAsync();
-    }
-
-    public async Task RunBotAsync()
-    {
-        key mykey = new key();
-        String key = mykey.serverKey;
-
+        //Initialise the bot client
         _client = new DiscordSocketClient();
 
+        //Event handlers registration
         _client.Log += LogAsync;
-
-        await _client.LoginAsync(TokenType.Bot, key);
-        await _client.StartAsync();
-
-        _client.Ready += () =>
-        {
-            Console.WriteLine("bot connected & ready");
-            return Task.CompletedTask;
-        };
-
-        _client.MessageReceived += async (message) =>
-        {
-            Console.WriteLine("message is: " + message.Content);
-
-            if (!message.Author.IsBot)
-            {
-                string msgContent = message.Content;
-                await message.Channel.SendMessageAsync(msgContent);
-            }
-        };
-
-
-        await Task.Delay(-1);
+        _client.Ready += ReadyAsync;
+        _client.MessageReceived += MessageReceivedAsync;
     }
 
-    private Task LogAsync(LogMessage log)
-    {
-        Console.WriteLine(log);
-        return Task.CompletedTask;
-    }
-
+ 
 }
